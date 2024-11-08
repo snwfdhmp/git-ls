8245a75f222fcf7b9e82b61808e19ec7009dfea1
snwfdhmp@nas.snwfdhmp.com <snwfdhmp@pm.me>
2024-11-08 19:06:12 +0100

    improve npm update script

--- main.js
+++ main.js
@@ -385,7 +385,9 @@ const doUpdate = async () => {
     console.log(pullResult.stdout.trim())
 
     try {
-      const npmResult = await execPromise(`npm install`, { cwd: __dirname })
+      const npmResult = await execPromise(`npm install  --no-audit`, {
+        cwd: __dirname,
+      })
       console.log(npmResult.stdout.trim())
     } catch (e) {
       console.log("npm install failed with following message:")
